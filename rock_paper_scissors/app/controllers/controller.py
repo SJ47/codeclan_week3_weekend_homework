2704766066c8e0e28ea4fe0d608db6b3c5babb50
Scott Johnston <scott@umachan.co.uk>
2021-01-24 13:48:53 +0000

    Some refactoring

--- rock_paper_scissors/app/controllers/controller.py
+++ rock_paper_scissors/app/controllers/controller.py
@@ -5,31 +5,36 @@ from app.models.player import Player
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    title = "Instructions"
+    return render_template("index.html", title=title)
 
 # Route for MVP portion of homework
 @app.route("/<player1_choice>/<player2_choice>")
 def get_player_choices(player1_choice, player2_choice):
+    title = "MVP Section"
+
     player1 = Player("Fred", player1_choice)
     player2 = Player("Barney", player2_choice)
 
     # Check for the winner
-    check_for_winner = Game()
-    winner = check_for_winner.select_winner(player1, player2)
+    # check_for_winner = Game()
+    # winner = check_for_winner.select_winner(player1, player2)
+    winner = Game.select_winner(player1, player2)
 
     # Return the result
-    return render_template("result.html", player1=player1, player2=player2, winner=winner)
+    return render_template("result.html", title=title, player1=player1, player2=player2, winner=winner)
 
 
 # Routes for extensions and further extensions of homework
 @app.route("/play")
 def play_game():
-    return render_template("play_game.html")
+    title = "Play Game"
+    return render_template("play_game.html", title=title)
 
 
 @app.route("/play", methods=["POST"])
 def get_player1_name():
-
+    title = "Play Game"
     player1=Player(request.form["player1_name"], request.form["player1_selection"])
 
     # Get computer player choice
@@ -43,5 +48,5 @@ def get_player1_name():
     winner = Game.select_winner(player1, player2)
 
     # return redirect(url_for("play_game", player1_name=player1_name))
-    return render_template("play_game.html", player1_name=player1.name, player1_choice=player1.choice, player2_name=player2.name, player2_choice=player2.choice, winner=winner)
-    # return render_template("play_game.html", player1=player1, winner=winner, player2=player2)
+    return render_template("play_game.html", title=title, player1_name=player1.name, player1_choice=player1.choice, player2_name=player2.name, player2_choice=player2.choice, winner=winner)
+    # return render_template("play_game.html", title=title, player1=player1, player2=player2, winner=winner)
--- rock_paper_scissors/app/models/game.py
+++ rock_paper_scissors/app/models/game.py
@@ -8,34 +8,26 @@ class Game:
     # Function will return the winner after comparing both players choices
     @classmethod    
     def select_winner(cls, player1, player2):
-        
-        # valid_choices = ["rock", "paper", "scissors"]
+        # Stop checking if either player made an invalid entry (for MVP exercise)     
+        if ((player1.choice.lower()) not in cls.valid_choices or (player2.choice.lower()) not in cls.valid_choices):
+            return "Invalid Choices Made - Try Again"
 
-        # Stop checking if either player made an invalid entry
-        if (player1.choice.lower()) not in cls.valid_choices:
-            return "Invalid Choices Made by Player 1 - Try Again"
-        
-        if (player2.choice.lower()) not in cls.valid_choices:
-            return "Invalid Choices Made by Player 2 - Try Again"
-        
-        ### Find the winner
-       
+        ### Find the winner      
         # check if it is a DRAW and return none - no winner
         if player1.choice.lower() == player2.choice.lower():
             return None
         
-        # Check for winner if player1 has ROCK
+        # default winner just to have a value
         winner = player1.name
-        if player1.choice.lower() == "rock":
-            if player2.choice.lower() == "paper":
+
+        # Check for winner if player1 has ROCK
+        if (player1.choice.lower() == "rock" and player2.choice.lower() == "paper"):
                 winner = player2.name
         # Check for winner if player1 has PAPER
-        elif player1.choice.lower() == "paper":
-            if player2.choice.lower() == "scissors":
+        elif (player1.choice.lower() == "paper" and player2.choice.lower() == "scissors"):
                 winner = player2.name
         # Check for winner if player1 has SCISSORS
-        elif player1.choice.lower() == "scissors":
-            if player2.choice.lower() == "rock":
+        elif (player1.choice.lower() == "scissors" and player2.choice.lower() == "rock"):
                 winner = player2.name
        
         # return winner 
@@ -44,6 +36,5 @@ class Game:
     # Return random choice for computer
     @classmethod
     def select_computer_random_choice(cls):
-        # valid_choices = ["rock", "paper", "scissors"]
         return random.choice(cls.valid_choices)
 
